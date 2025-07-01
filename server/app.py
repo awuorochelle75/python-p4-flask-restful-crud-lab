bd6f34ab9cbf44762e57ee28d73fad20ef236a17
Rochelle Awuor <awuorochelle75@gmail.com>
2025-07-01 08:33:32 +0300

    modifies my files

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
-
+# ---------- /plants ----------
 class Plants(Resource):
 
     def get(self):
@@ -37,19 +37,40 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
-
+# ---------- /plants/<id> ----------
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response({'error': 'Plant not found'}, 404)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        data = request.get_json()
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+            db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
-
+# ---------- Run ----------
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
